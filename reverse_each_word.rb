ef709ec5f53206b87be5faac335c12e32792712f
Gilberto Ocampo <ocampo@gate.net>
2020-02-21 19:42:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-# def reverse_each_word(string1)
-  # string1.reverse!
-  # array1 = string1.split
+# def reverse_each_word(sentence1)
+  # sentence1.reverse!
+  # array1 = sentence1.split
   # array2 = array1.reverse
-  # string2 = array2.join " "
+  # sentence2 = array2.join " "
 # end
 
 
-def reverse_each_word(string1)
-  array1 = string1.split
+def reverse_each_word(sentence1)
+  array1 = sentence1.split
   array1.each do |n,idx|
     array1[(idx)].reverse!
   end
