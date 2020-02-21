1069234299001c4592645036413b9ddc752641cd
Gilberto Ocampo <ocampo@gate.net>
2020-02-21 19:12:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,8 @@
 def reverse_each_word(string1)
   array1 = string1.split
   array2 = []
-  array1.each do |word|
-    array2 = array1.reverse!
+  array1.each do |n|
+    array2(n) = array1(n).reverse!
   end
   string2 = array2.join " "
 end
