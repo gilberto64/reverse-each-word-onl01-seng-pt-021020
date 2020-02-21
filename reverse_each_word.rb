63d4946297375e16491f1fd65a7bc77e2d9a8b5e
Gilberto Ocampo <ocampo@gate.net>
2020-02-21 19:19:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,12 +5,10 @@
   # string2 = array2.join " "
 # end
 
-require.pry
 
 def reverse_each_word(string1)
   array1 = string1.split
   array2 = []
-  binding.pry 
   array1.each do |n|
     array2(n) << array1(n).reverse!
   end
